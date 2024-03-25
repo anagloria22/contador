1a267054cffd6541bba6ab774561815766de6b61
Ana Gloria <147454263+anagloria22@users.noreply.github.com>
2024-03-25 17:25:51 -0300

    att

---
+++ script.js
@@ -0,0 +1,20 @@
+function contar () {
+    let ini = document.getElementById('iinicio')
+    let fim = document.getElementById('ifim')
+    let passo = document.getElementById('ipasso')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        for(let c = i; c <= f; c += p) {
+            res.innerHTML += ` ${c}\u{1F449}`
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
