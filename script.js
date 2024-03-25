4b0102e1d412cd7d28bd6a597470c99d9fd2b1e6
Ana Gloria <147454263+anagloria22@users.noreply.github.com>
2024-03-25 17:34:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,10 +11,15 @@ function contar () {
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-
-        for(let c = i; c <= f; c += p) {
-            res.innerHTML += ` ${c}\u{1F449}`
-        }
-        res.innerHTML += `\u{1F3C1}`
+        if (i < f) { //contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c}\u{1F449}`
+            }
+        } else { //contagem regressiva
+            for(c = i; c >= f; c-= p) {
+                res.innerHTML += `${c}\u{1F449}`
+            }
+        }   res.innerHTML += `\u{1F3C1}`
+        
     }
 }
